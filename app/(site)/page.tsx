c2d5cdbc8855da2015f3bca8b6c6d40b216dbd38
DavidSGS <davidgarcia200124@gmail.com>
2023-11-04 18:35:11 -0500

    Slidebar hecha, SlideBarItems Hecha, Box hecha Estilos globales hecha, y el layout esta hecho

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,31 @@
+import Header from "@/components/Header";
+
 export default function Home() {
-  return <div> Hola (site)</div>;
+  return (
+    <div
+      className="        
+      bg-neutral-900 
+      rounded-lg 
+      h-full 
+      w-full 
+      overflow-hidden 
+      overflow-y-auto
+      "
+    >
+      <Header>{}</Header>
+      <div>
+        <div className="mb-2">
+          <h1
+            className="
+            text-white 
+              text-3xl 
+              font-semibold
+            "
+          >
+            Welcome back
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { twMerge } from "tailwind-merge";
+interface HeaderProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const router = useRouter();
+  const handleLogout = () => {};
+
+  return (
+    <div
+      className={twMerge(`
+
+  `)}
+    ></div>
+  );
+};
+
+export default Header;
