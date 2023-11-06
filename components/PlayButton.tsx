ee286252e54af075a7bd1f6001e8a07d5f22db98
DavidSGS <davidgarcia200124@gmail.com>
2023-11-06 15:21:28 -0500

    Se agrego el boton de play

--- components/PlayButton.tsx
+++ components/PlayButton.tsx
@@ -2,7 +2,23 @@ import { FaPlay } from "react-icons/fa";
 
 const PlayButton = () => {
   return (
-    <button>
+    <button
+      className="
+        transition
+        opacity-0
+        rounded-full
+        flex
+        items-center
+        bg-purple-500
+        p-4
+        drop-shadow-md
+        translate
+        translate-y-1/4
+        group-hover:opacity-100
+        group-hover:translate-y-0
+        group-hover:scale-110
+    "
+    >
       <FaPlay />
     </button>
   );
