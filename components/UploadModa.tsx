4bd207e9726e354bae306906a026091b8bf11994
DavidSGS <davidgarcia200124@gmail.com>
2023-11-05 19:00:36 -0500

    Se agrega la primeras partes para agregar musica

--- components/UploadModa.tsx
+++ components/UploadModa.tsx
@@ -1,16 +1,27 @@
 "use client";
 import useUploadModal from "@/hooks/useUploadModal";
 import Modal from "./Modal";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 const UploadModalComp = () => {
+  const { register, handleSubmit, reset } = useForm<FieldValues>({
+    defaultValues: {
+      author: "",
+      title: "",
+      song: null,
+      image: null,
+    },
+  });
   const uploadModal = useUploadModal();
 
   const onChange = (open: boolean) => {
     if (!open) {
+      reset();
       uploadModal.onClose();
     }
   };
-  
+  const onSubmit: SubmitHandler<FieldValues> = async(values) => {};
+
   return (
     <Modal
       title="Add a song"
@@ -18,6 +29,7 @@ const UploadModalComp = () => {
       isOpen={uploadModal.isOpen}
       onChange={onChange}
     >
+      <form onClick={handleSubmit(onSubmit)}></form>
       Upload Content
     </Modal>
   );
