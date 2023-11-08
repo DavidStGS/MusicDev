fc7b291a5466680cf741d1bb0d408b372baa72b3
DavidSGS <davidgarcia200124@gmail.com>
2023-11-08 03:04:10 -0500

    Se agrega el manifesto y se corrgie el color

--- app/manifest.ts
+++ app/manifest.ts
@@ -8,12 +8,12 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: '/', 
     display: 'standalone',
     background_color: '#171717',
-    theme_color: '#171717',
+    theme_color: '#00000',
     icons: [
       {
         src: '/favicon.ico',
-        sizes: 'any',
-        type: 'image/x-icon',
+        sizes: '144px',
+        type: 'image/svg',
       },
     ],
   }
