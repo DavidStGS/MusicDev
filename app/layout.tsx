9c802c8c2b3ee79fe4fd1722fdb8799944059efc
DavidSGS <davidgarcia200124@gmail.com>
2023-11-11 21:09:12 -0500

    Adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,13 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8563047377957074"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body className={font.className}>
         <ToasterProvider />
         <SupabaseProvider>
