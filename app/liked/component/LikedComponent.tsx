221b12e6751d952b2d78642cc9362bd7de91dce7
DavidSGS <davidgarcia200124@gmail.com>
2023-11-15 18:04:14 -0500

    pagina liked terminada

--- app/liked/component/LikedComponent.tsx
+++ app/liked/component/LikedComponent.tsx
@@ -1,16 +1,72 @@
 "use client";
 
+import { useUser } from "@/hooks/useUsers";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import { Song } from "@/types";
+import MediaItem from "@/components/MediaItem";
+import LikeButton from "@/components/LikeButton";
+import useOnPlay from "@/hooks/useOnPLay";
 
 interface LikedComponentProps {
   songs: Song[];
 }
 
 const LikedComponent: React.FC<LikedComponentProps> = ({ songs }) => {
-    
+  const router = useRouter();
+  const { isLoading, user } = useUser();
+  const onPlay = useOnPlay(songs);
+
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.replace("/");
+    }
+  }, [isLoading, user, router]);
+
+  if (songs.length === 0) {
+    return (
+      <div
+        className="
+            flex
+            flex-col
+            gap-y-2
+            w-full
+            px-6
+            text-neutral-400
+        "
+      >
+        No liked Songs
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <div>asd</div>
+    <div
+      className="
+        flex
+        flex-col
+        gap-y-2
+        w-full
+        p-6
+    "
+    >
+      {" "}
+      {songs.map((song) => (
+        <div
+          key={song.id}
+          className="
+                flex
+                items-center
+                gap-x-10
+                w-full
+            "
+        >
+          <div className="flex-1">
+            <MediaItem onClick={(id: string) => onPlay(id)} data={song} />
+          </div>
+          <LikeButton songId={song.id} />
+        </div>
+      ))}
     </div>
   );
 };
